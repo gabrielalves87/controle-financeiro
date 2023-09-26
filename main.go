65b4428c8da45038083f93046a6c6f7b29d79997
gabrielofers <30701486+gabrielofers@users.noreply.github.com>
2023-09-26 17:19:29 +0000

    vai

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"errors"
+	"fmt"
+)
+
+func message(texto string) (msg string, err error){
+	mensagem := texto
+	if mensagem == ""{
+		err := errors.New("ERROR")
+		//msg := "mensagem em branco favor informa a mensagem"
+		return "", err
+	}
+	return mensagem, nil
+}
+
+func main(){
+	vanilaSemErro, err := message("Ola")
+	if err !=nil {
+		os.Exit(1)
+	}
+	vanilaComErro, err := message("")
+	fmt.Println(vanilaSemErro)
+	fmt.Println(vanilaComErro)
+	fmt.Println("Ola Mundo")
+}
