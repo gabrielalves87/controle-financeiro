f50ef8b56d1f2afa34d4f82d89885c63c97235c4
gabrielofers <30701486+gabrielofers@users.noreply.github.com>
2023-09-27 15:42:28 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,63 @@
 package main
 
 import (
-	"os"
-	"errors"
+	"context"
 	"fmt"
+	"os"
+	monitoringclient "github.com/prometheus-operator/prometheus-operator/pkg/client/versioned"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	mfc "github.com/manifestival/client-go-client"
 )
 
-func message(texto string) (msg string, err error){
-	mensagem := texto
-	if mensagem == ""{
-		err := errors.New("ERROR")
-		//msg := "mensagem em branco favor informa a mensagem"
-		return "", err
+
+
+
+func main() {
+	kubeconfigPath := os.Getenv("KUBECONFIG")
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	mclient, err := monitoringclient.NewForConfig(config)
+	if err != nil {
+		panic(err)
 	}
-	return mensagem, nil
+	promRules, err := mclient.MonitoringV1().PrometheusRules("tks-system").List(context.TODO(), v1.ListOptions{})
+	for _ , rule := range promRules.Items{
+		fmt.Println(rule.Name)
+	}
 }
 
-func main(){
-	vanilaSemErro, err := message("Ola")
-	if err !=nil {
-		os.Exit(1)
+func ApplyManifest(manisfestPath string) {
+	client, err := GetKubeClientFromKubeConfig()
+	if err != nil {
+		//panic("Failed to load manifest")
+		panic("Unable get kube credintials")
 	}
-	vanilaComErro, err := message("")
-	fmt.Println(vanilaSemErro)
-	fmt.Println(vanilaComErro)
-	fmt.Println("Ola Mundo")
+	manifest, err := mfc.NewManifest(manisfestPath, client)
+	if err != nil {
+		panic("Failed to load manifest")
+	}
+	err = manifest.Apply()
+	if err != nil {
+		panic("Failed apply manifest")
+	}
+}
+
+func GetKubeClientFromKubeConfig() (*rest.Config, error) {
+	kubeconfigPath := os.Getenv("KUBECONFIG")
+	if kubeconfigPath != "" {
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			return nil, fmt.Errorf("unable to load kubeconfig from %s: %v", kubeconfigPath, err)
+		}
+		//kubeconfig = config
+		return config, nil
+	}
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("unable to load in-cluster config: %v", err)
+	}
+
+	return config, nil
 }
