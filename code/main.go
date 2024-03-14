a10f9cc1201034710fd2b8cd4e4fc0c19ea3fe2f
Gabriel Alves <gabrielofers@gmail.com>
2024-03-14 17:13:44 -0300

    testes

--- code/main.go
+++ code/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/gabrielalves87/controle-financeiro/database"
+	"github.com/gabrielalves87/controle-financeiro/models"
 )
 
 func main() {
 	database.ConexaoComBancoDeDados()
-	fmt.Println("Tudo bem!!!")
+	log.Println("Tudo bem!!!")
+	usuario := models.User{Username: "Gabriel", Password: "batatinha", Email: "gabriel.alves@testes.com.br"}
+	err := database.DB.Create(usuario)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("Usuario criado com sucesso")
+
 }
