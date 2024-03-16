da041863dfa1798e97598e6ac49a3c6fd40a28c1
Gabriel Alves <gabrielofers@gmail.com>
2024-03-16 17:53:57 -0300

    Criando Swager

--- code/controllers/controller.go
+++ code/controllers/controller.go
@@ -2,8 +2,20 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/swaggo/swag/example/celler/httputil"
 )
 
+// @BasePath /
+
+// ExibeTodosAlunos godoc
+// @Summary ExibeTodosAlunos example
+// @Schemes
+// @Description do pinExibeTodosAlunosg
+// @Tags ExibeTodosAlunos
+// @Accept json
+// @Produce json
+// @Success 200 {string} ExibeTodosAlunos
+// @Router /alunos [get]
 func ExibeTodosAlunos(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"id":   "1",
@@ -12,6 +24,19 @@ func ExibeTodosAlunos(c *gin.Context) {
 
 }
 
+// @BasePath /
+
+// Saudacao godoc
+// @Summary Saudacao example
+// @Schemes
+// @Description do Saudacao
+// @Tags Saudacao
+// @Accept json
+// @Param name path string true "Nome"
+// @Produce json
+// @Success 200 {string} Saudacao
+// @Failure 400 {object} httputil.HTTPError
+// @Router /alunos/{name} [get]
 func Saudacao(c *gin.Context) {
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
--- code/routes/routes.go
+++ code/routes/routes.go
@@ -3,12 +3,17 @@ package routes
 import (
 	"github.com/gabrielalves87/controle-financeiro/controllers"
 	"github.com/gin-gonic/gin"
+	docs "github.com/gabrielalves87/controle-financeiro/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Handlerequest() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/alunos/:nome", controllers.Saudacao)
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.POST("/user", controllers.CriaNovoUsuario)
 	r.POST("/categorie", controllers.CriaNovaCategoria)
 	r.Run("0.0.0.0:3000")
