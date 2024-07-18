99116ed7423abf3fb67460cbb016f4d723977d32
Gabriel Alves <gabrielofers@gmail.com>
2024-07-17 21:27:59 -0300

    routes api

--- code/api/server.go
+++ code/api/server.go
@@ -1,31 +1,43 @@
 package api
+
 import (
 	db "github.com/gabrielalves87/controle-financeiro/db/sqlc"
 	"github.com/gin-gonic/gin"
 )
 
-type Server struct{
-	store *db.SQLStore
+type Server struct {
+	store  *db.SQLStore
 	router *gin.Engine
 }
 
-func NewServer(store *db.SQLStore) *Server{
-	server := &Server{store:store}
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{store: store}
 	router := gin.Default()
 
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
 	router.GET("/user/id/:id", server.getUserbyId)
+
+	router.POST("/category", server.createCategory)
+	router.GET("/category/id/:id", server.getCategory)
+	router.GET("/category", server.getCategories)
+	router.DELETE("/category/:id", server.deleteCategory)
+	router.PUT("/category/:id", server.updateCategory)
+
+	router.POST("/account", server.createAccount)
+	router.GET("/account/id/:id", server.getAccount)
+	router.DELETE("/account/:id", server.deleteAccount)
+	router.PUT("/account/:id", server.updateAccount)
+	router.GET("/account/", server.getAccounts)
 	server.router = router
 	return server
 }
 
-
-func(server *Server) Start(address string) error {
+func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
 
-func errorResponse(err error) gin.H{
+func errorResponse(err error) gin.H {
 	return gin.H{
 		"api has error:": err.Error(),
 	}
--- code/db/sqlc/categories.sql.go
+++ code/db/sqlc/categories.sql.go
@@ -57,13 +57,13 @@ func (q *Queries) DeleteCategory(ctx context.Context, id int32) error {
 
 const getCategories = `-- name: GetCategories :many
 SELECT id, user_id, title, type, description, created_at FROM categories
-WHERE user_id = $1 AND type = $2 
-AND title LIKE $3 AND description like $4
+WHERE user_id = $1 OR type = $2 
+OR title LIKE $3 OR description like $4
 `
 
 type GetCategoriesParams struct {
 	UserID      int32       `json:"user_id"`
-	Type        string `json:"type"`
+	Type        string 		`json:"type"`
 	Title       string      `json:"title"`
 	Description string      `json:"description"`
 }
