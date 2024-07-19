ebf1feb6cd1deca694cd95a2bf61dc5d44974820
Gabriel Alves <gabrielofers@gmail.com>
2024-07-19 08:37:42 -0300

    using godotenv

--- code/main.go
+++ code/main.go
@@ -1,34 +1,38 @@
 package main
 
 import (
-	"log"
 	"database/sql"
-	db "github.com/gabrielalves87/controle-financeiro/db/sqlc"
-	_ "github.com/lib/pq"
+	"log"
+	"os"
+
 	"github.com/gabrielalves87/controle-financeiro/api"
+	db "github.com/gabrielalves87/controle-financeiro/db/sqlc"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	// "github.com/gabrielalves87/controle-financeiro/database"
 	// "github.com/gabrielalves87/controle-financeiro/routes"
 )
 
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
-	serverAddress string = "0.0.0.0:8000"
-)
-
 func main() {
 	// database.ConexaoComBancoDeDados()
 	// log.Println("Tudo bem!!!")
 	// routes.Handlerequest()
-	conn, err := sql.Open(dbDriver,dbSource)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	  }
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 	err = server.Start(serverAddress)
-	if err != nil{
+	if err != nil {
 		log.Fatal("cannot start api: ", err)
 	}
 }
